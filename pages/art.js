ecdd37b98bcdba6aabd42e0141bdb0807f7a606d
Chris Thomas <chrissssthomas@gmail.com>
2022-02-15 16:56:29 +0000

    fixed mobile views and swiping

--- pages/art.js
+++ pages/art.js
@@ -1,31 +1,23 @@
-import Head from "next/head";
-import Image from "next/image";
-import artworks from "../data/art.json";
-import ImageGallery from "../components/lightbox";
+import artworks from "../data/art.json"
+import ImageGallery from "../components/lightbox"
 
 const Art = () => {
-  return (
-    <div>
-      {" "}
-      <div>
-        <div className="mx-auto max-w-[1250px]">
-          <div className="flex justify-between py-4 border">
-            <h3>Kemi Mai</h3>
-            <button>Mate</button>
-          </div>
-          <button>
-            <span class="material-icons-outlined">arrow_forward_ios</span>
-          </button>
-          <h3 className="text-2xl font-medium pt-24 pb-6">Selected works</h3>
-          <div>
+    return (
+        <div>
             <div>
-              <ImageGallery images={artworks.art} />
+                <div className="mx-auto px-4">
+                    <h3 className="text-2xl font-medium pt-24 px-5 sm:px-0 pb-6">
+                        Selected works
+                    </h3>
+                    <div>
+                        <div className="mb-12">
+                            <ImageGallery images={artworks.art} />
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
-};
+    )
+}
 
-export default Art;
+export default Art
--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,44 @@
-import caseStudies from "../data/caseStudies.json";
-import { useState } from "react";
+import caseStudies from "../data/caseStudies.json"
+import { useState } from "react"
 
 export default function Home() {
-  const [currentTab, setCurrentTab] = useState("home");
+    const [currentTab, setCurrentTab] = useState("home")
 
-  return (
-    <div>
-      <main>
-        <div className="flex items-center justify-center border h-screen">
-          <h1 className="text-5xl">
-            Donec accumsan, vitae, erat pharetra mollis ultrices.{" "}
-          </h1>
-        </div>
-
-        <div className="px-8 max-w-[1250px] mx-auto">
-          {caseStudies.studies.map((study, index) => (
-            <div key="{study}" className="pb-16 ">
-              {/* <Image src={require(study.image)} /> */}
-              <img src={study.image} alt={study.title} />
-              <div className="py-16 flex">
-                <h3 className="text-4xl w-1/2 font-semibold">{study.title}</h3>
-                <div className="w-1/2">
-                  <div className="w-full flex pb-6 justify-between font-medium text-2xl">
-                    <span>Case study </span>
-                    <button className="font-medium hover:text-gray-500">
-                      Expand
-                    </button>
-                  </div>
-                  <div>
-                    <p className="text-lg">{study.description} </p>
-                  </div>
+    return (
+        <div>
+            <main>
+                <div className="flex items-center justify-center  h-screen">
+                    <h1 className="text-5xl">
+                        Donec accumsan, vitae, erat pharetra mollis ultrices.{" "}
+                    </h1>
                 </div>
-              </div>
-            </div>
-          ))}
+
+                <div className="px-8 max-w-[1250px] mx-auto">
+                    {caseStudies.studies.map((study, index) => (
+                        <div key="{study}" className="pb-16 ">
+                            <img src={study.image} alt={study.title} />
+                            <div className="py-16 flex">
+                                <h3 className="text-4xl w-1/2 font-semibold">
+                                    {study.title}
+                                </h3>
+                                <div className="w-1/2">
+                                    <div className="w-full flex pb-6 justify-between font-medium text-2xl">
+                                        <span>Case study </span>
+                                        <button className="font-medium hover:text-gray-500">
+                                            Expand
+                                        </button>
+                                    </div>
+                                    <div>
+                                        <p className="text-lg">
+                                            {study.description}{" "}
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </main>
         </div>
-      </main>
-    </div>
-  );
+    )
 }
